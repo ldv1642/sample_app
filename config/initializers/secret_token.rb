a323ea7a03109fccbb94e7e7cc8ffa49b9bda351
ldv1642 <ldv1642>
2012-04-02 14:55:38 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1148477aaf7c47a3a240be555eb7324e8120dacba9ed27cc8c5890c86f6a8d91ce390ef876346396aeadc679934b42b73eadde273f2795681267ddb2208bced0'
